cfbe38b29d4776d2f0d102bbe0e8361bd6b499a8
Erikson <perezerikson16gmail.com>
2024-07-15 20:08:43 -0400

    Segundo Archivo

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
+    'Si te duele, hazlo más a menudo.',
+    '¡Agregar mano de obra a un proyecto de software tardío lo hace más tarde!',
+    'El primer 90 por ciento del código representa el primer 10 por ciento del tiempo de desarrollo... El 10 por ciento restante del código representa el otro 90 por ciento del tiempo de desarrollo.',
+    'Cualquier tonto puede escribir código que una computadora pueda entender. Los buenos programadores escriben código que los humanos pueden entender.',
+    'La optimización prematura es la fuente de todos los males.',
+    'La depuración es dos veces más difícil que escribir el código en primer lugar. Por lo tanto, si escribe el código de la forma más inteligente posible, por definición no será lo suficientemente inteligente como para depurarlo.',
+    'Programar sin un uso extremadamente intensivo de console.log es lo mismo que si un médico se negara a utilizar rayos X o análisis de sangre al diagnosticar a sus pacientes.',
+    "La única manera de ir rápido es hacerlo bien"
   ]
 
   const [selected, setSelected] = useState(0)
@@ -33,10 +33,10 @@ const App = () => {
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={handleVote}>vote</button>
-      <button onClick={handleNextAnecdote}>next anecdote</button>
-      <h2>Anecdote with most votes</h2>
+      <button onClick={handleNextAnecdote}>siguiente anécdota</button>
+      <h2>Anécdota con más votos</h2>
       <p>{anecdotes[mostVotedAnecdoteIndex]}</p>
-      <p>has {votes[mostVotedAnecdoteIndex]} votes</p>
+      <p>Tiene {votes[mostVotedAnecdoteIndex]} votes</p>
     </div>
   )
 }
